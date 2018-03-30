17e8fc572818cfc93b917a0af62e36c00104d71b
awijasa <awijasa@gmail.com>
2018-03-30 11:35:05 -0500

    In the backend, Assigned To fields end with [assigned_to][]
    instead of the expected [assigned_to].
    
    To work with this line in edit-project.js:
    
    theColumn = $('[name$="['+ filter.column +']"]', tr);
    
    The data-column needs to be changed from assigned_to to assigned_to][

--- src/includes/admin/metaboxes/class-up-metaboxes-projects.php
+++ src/includes/admin/metaboxes/class-up-metaboxes-projects.php
@@ -434,7 +434,7 @@ class UpStream_Metaboxes_Projects {
           </div>
           <div class="up-c-filter">
             <label for="<?php echo $prefix . 'assignee'; ?>" class="up-s-mb-2"><?php _e('Assignee', 'upstream'); ?></label>
-            <select id="<?php echo $prefix . 'assignee'; ?>" class="up-o-filter o-select2" data-column="assigned_to" data-placeholder="" multiple>
+            <select id="<?php echo $prefix . 'assignee'; ?>" class="up-o-filter o-select2" data-column="assigned_to][" data-placeholder="" multiple>
               <option></option>
               <option value="<?php echo $currentUserId; ?>"><?php _e('Me', 'upstream'); ?></option>
               <option value="__none__"><?php _e('Nobody', 'upstream'); ?></option>
@@ -502,7 +502,7 @@ class UpStream_Metaboxes_Projects {
           </div>
           <div class="up-c-filter">
             <label for="<?php echo $prefix . 'assignee'; ?>" class="up-s-mb-2"><?php _e('Assignee', 'upstream'); ?></label>
-            <select id="<?php echo $prefix . 'assignee'; ?>" class="up-o-filter o-select2" data-column="assigned_to" multiple data-placeholder="">
+            <select id="<?php echo $prefix . 'assignee'; ?>" class="up-o-filter o-select2" data-column="assigned_to][" multiple data-placeholder="">
               <option></option>
               <option value="<?php echo $currentUserId; ?>"><?php _e('Me', 'upstream'); ?></option>
               <option value="__none__"><?php _e('Nobody', 'upstream'); ?></option>
@@ -585,7 +585,7 @@ class UpStream_Metaboxes_Projects {
           </div>
           <div class="up-c-filter">
             <label for="<?php echo $prefix . 'assignee'; ?>" class="up-s-mb-2"><?php _e('Assignee', 'upstream'); ?></label>
-            <select id="<?php echo $prefix . 'assignee'; ?>" class="up-o-filter o-select2" data-column="assigned_to" data-placeholder="" multiple>
+            <select id="<?php echo $prefix . 'assignee'; ?>" class="up-o-filter o-select2" data-column="assigned_to][" data-placeholder="" multiple>
               <option></option>
               <option value="<?php echo $currentUserId; ?>"><?php _e('Me', 'upstream'); ?></option>
               <option value="__none__"><?php _e('Nobody', 'upstream'); ?></option>
